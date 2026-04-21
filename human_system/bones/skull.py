1d7bb0d361d0a87ccc2ed8868160bf223732f670
alexmacleodreborn-crypto <alexmacleodreborn@gmail.com>
2026-04-21 15:45:41 +0100

    Add skull bones module

---
+++ human_system/bones/skull.py
@@ -0,0 +1,29 @@
+"""
+Skull Bones (Cranial + Facial)
+"""
+
+SKULL_BONES = {
+    1: "frontal",
+    2: "parietal_L",
+    3: "parietal_R",
+    4: "temporal_L",
+    5: "temporal_R",
+    6: "occipital",
+    7: "sphenoid",
+    8: "ethmoid",
+
+    9: "nasal_L",
+    10: "nasal_R",
+    11: "maxilla_L",
+    12: "maxilla_R",
+    13: "zygomatic_L",
+    14: "zygomatic_R",
+    15: "palatine_L",
+    16: "palatine_R",
+    17: "lacrimal_L",
+    18: "lacrimal_R",
+    19: "inferior_nasal_concha_L",
+    20: "inferior_nasal_concha_R",
+    21: "vomer",
+    22: "mandible"
+}
