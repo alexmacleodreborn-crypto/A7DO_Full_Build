980a48b2cdbd28451095e6c2d043b9405835174e
alexmacleodreborn-crypto <alexmacleodreborn@gmail.com>
2026-04-21 15:46:56 +0100

    Add upper limb bones module

---
+++ human_system/bones/upper_limbs.py
@@ -0,0 +1,17 @@
+"""
+Upper Limb Bones
+"""
+
+UPPER_LIMB_BONES = {
+    81: "clavicle_L",
+    82: "clavicle_R",
+    83: "scapula_L",
+    84: "scapula_R",
+
+    85: "humerus_L",
+    86: "humerus_R",
+    87: "radius_L",
+    88: "radius_R",
+    89: "ulna_L",
+    90: "ulna_R",
+}
