e3f948a73d00e3c009bb83ecd6d3f58f7fbd7aa5
alexmacleodreborn-crypto <alexmacleodreborn@gmail.com>
2026-04-21 15:47:16 +0100

    Add lower limb bones module

---
+++ human_system/bones/lower_limbs.py
@@ -0,0 +1,17 @@
+"""
+Lower Limb Bones
+"""
+
+LOWER_LIMB_BONES = {
+    145: "hip_L",
+    146: "hip_R",
+
+    147: "femur_L",
+    148: "femur_R",
+    149: "patella_L",
+    150: "patella_R",
+    151: "tibia_L",
+    152: "tibia_R",
+    153: "fibula_L",
+    154: "fibula_R",
+}
